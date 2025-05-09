45e79aac244e565bd88e09dcc877f50abd505de0
tobi132 <tobi132@gmx.net>
2025-05-09 08:01:16 +0200

    Create translate.php

---
+++ plugins/theme-canary/themes/canary/boxes/translate.php
@@ -0,0 +1,6 @@
+<div id="google_translate_element"></div><script type="text/javascript">
+	function googleTranslateElementInit() {
+		new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,pl,pt'}, 'google_translate_element');
+	}
+</script>
+<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
