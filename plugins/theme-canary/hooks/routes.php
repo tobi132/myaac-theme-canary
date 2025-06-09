ecfc0340fc624150bd970734702ae6abfb1efdcd
tobi132 <tobi132@gmx.net>
2025-06-09 20:48:17 +0200

    v1.0.2 - Adjust characters route to allow squared brackets []

--- plugins/theme-canary/hooks/routes.php
+++ plugins/theme-canary/hooks/routes.php
@@ -4,5 +4,5 @@ defined('MYAAC') or die('Direct access not allowed!');
 global $template_name;
 
 if ($template_name === 'canary') {
-	$args[] = [['get', 'post'], 'characters[/{name:string}]', 'plugins/theme-canary/themes/canary/pages/characters.php', 500];
+	$args[] = [['get', 'post'], 'characters[/{name:[A-Za-z0-9-_%+\' \[\]]+}]', 'plugins/theme-canary/themes/canary/pages/characters.php', 500];
 }
