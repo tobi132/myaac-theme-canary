85e1c2a0fb1d26afd2354b582c85330347f483b6
tobi132 <tobi132@gmx.net>
2025-10-25 00:56:49 +0200

    v1.2 - event-schedule overhaul
    * display all months - previous/next switch
    * change first day to Monday (instead of Sunday)
    * display star on start/end event
    * sorting by "displaypriority"
    * optimize events.xml loading

--- plugins/theme-canary/themes/canary/pages/event-schedule.php
+++ plugins/theme-canary/themes/canary/pages/event-schedule.php
@@ -98,98 +98,101 @@
 defined('MYAAC') or die('Direct access not allowed!');
 $title = 'Event Schedule';
 
+$currentYear = date('Y');
+$currentMonth = date('n');
+
+$getYear	= $_GET['year'] ?? $currentYear;
+$getMonth	= $_GET['month'] ?? $currentMonth;
+
+$dateObj	= DateTime::createFromFormat('!m', $getMonth);
+$monthName	= $dateObj->format('F'); // March
+
 function showWeeks(): string
 {
 	$out = "";
-	$weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	$weeks = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 	for ($i = 0; $i < 7; $i++) $out .= "<td>$weeks[$i]</td>";
 	return $out;
 }
 
-function getAmountDays(string $month): int
+function generateIndicator($event, $currentDay): string
 {
-	$amountDays = [
-	'01' => 31, '02' => 28, '03' => 31, '04' => 30, '05' => 31, '06' => 30,
-	'07' => 31, '08' => 31, '09' => 30, '10' => 31, '11' => 30, '12' => 31
-	];
+	$isStartOrEnd = '';
 
-	if (((date('Y') % 4) == 0 and (date('Y') % 100) != 0) or (date('Y') % 400) == 0) {
-		$amountDays['02'] = 29;  // altera o numero de dias de fevereiro se o ano for bissexto
+	$explodeStartDate = explode('/', $event['startdate']);
+	$explodeEndDate = explode('/', $event['enddate']);
+
+	if ($currentDay == $explodeStartDate[1] ||
+		$currentDay == $explodeEndDate[1]
+	) {
+		$isStartOrEnd = '*';
 	}
 
-	return $amountDays[$month];
-}
-
-function getMonthName($month): string
-{
-	$months = [
-	'01' => "January", '02' => "February", '03' => "March",
-	'04' => "April", '05' => "May", '06' => "June",
-	'07' => "July", '08' => "August", '09' => "September",
-	'10' => "October", '11' => "November", '12' => "December"
-	];
-
-	return ($month >= 01 && $month <= 12) ? $months[$month] : "Unknown month";
-}
-
-function generateIndicator($event): string
-{
 	$out = "<span style='width: 120px;' class='HelperDivIndicator'";
 	$div = "<div class='activated'>{$event['name']}:</div><div style='margin-bottom: 20px'>&amp;bull; {$event->description['description']}</div>";
 	$out .= 'onmouseover="ActivateHelperDiv($(this), &quot;&quot;, &quot;' . $div . '&quot;, &quot;&quot;);"';
 	$out .= 'onmouseout="$(&quot;#HelperDivContainer&quot;).hide();">';
-	$out .= "<div class='event_name' style='background: {$event->colors['colordark']};'>{$event['name']}</div></span>";
+	$out .= "<div class='event_name' style='background: {$event->colors['colordark']};'>{$isStartOrEnd}{$event['name']}</div></span>";
 
 	return $out;
 }
 
-function showCalendar($month = null): string
+function showCalendar($month, $year): string
 {
-	$month = $month ?: date('m');
-	$amountDays = getAmountDays(str_pad($month, 2, '0', STR_PAD_LEFT));
+	$amountDays = date('t', mktime(0, 0, 0, $month, 1, $year));
 	$currentDay = 0;
 
-	$dayOfWeek = jddayofweek(cal_to_jd(CAL_GREGORIAN, $month, "01", date('Y')), 0);
+	$firstDayOfWeek = jddayofweek(cal_to_jd(CAL_GREGORIAN, $month, "01", $year), 0) - 1;
 
 	$outDays = "<tr style='text-align:center; width:120px; background-color:#5f4d41;'>" . showWeeks() . "</tr>";
+
+	$events_xml = config('data_path') . 'XML/events.xml';
+	if (file_exists($events_xml)) {
+		$xml = simplexml_load_file($events_xml);
+
+		$events = [];
+		foreach ($xml->event as $event) {
+			$events[] = $event;
+		}
+
+		function compareEvent($obj1, $obj2): int {
+			return ((int)$obj1->details['displaypriority'] <=> (int)$obj2->details['displaypriority']);
+		}
+
+		usort($events, 'compareEvent');
+	}
 
 	for ($row = 0; $row < 5; $row++) {
 		$outDays .= "<tr>";
 		for ($column = 0; $column < 7; $column++) {
 			$outDays .= "<td style='height:82px; background-clip: padding-box; overflow: hidden; vertical-align:top;' ";
 			$color = "other_day";
-			if (($currentDay == (date('d') - 1) && date('m') == $month)) {
+			if ($currentDay == (date('d') - 1) && date('m') == $month) {
 				$color = "today";
 			} else {
 				if (($currentDay + 1) <= $amountDays) {
-					$color = ($column < $dayOfWeek && $row == 0) ? "other_day" : "default";
+					$color = ($column < $firstDayOfWeek && $row == 0) ? "other_day" : "default";
 				}
 			}
 
 			$outDays .= "id='$color'>";
 
 			if ($currentDay + 1 <= $amountDays) {
-				if ($column < $dayOfWeek && $row == 0) {
+				if ($column < $firstDayOfWeek && $row == 0) {
 					$outDays .= " ";
 				} else {
-					$outDays .= "<div class='day'><span style='vertical-align: text-bottom;'>" . ++$currentDay . " <img style='border:0;' src='https://static.tibia.com/images/global/content/icon-seasonal.png'></span></div>";
+					$outDays .= "<div class='day'><span style='vertical-align: text-bottom;'>" . ++$currentDay . " </span></div>";
 
-					global $config;
-					$events_xml = $config['data_path'] . 'XML/events.xml';
-					if (file_exists($events_xml)) {
-						$xml = simplexml_load_file($events_xml);
+					if (isset($events)) {
+						$current_date = "$month/$currentDay/$year";
 
-						$currentDay = str_pad($currentDay, 2, '0', STR_PAD_LEFT);
-
-						$verif_date = "$month/$currentDay/" . date('Y');
-
-						foreach ($xml->event as $event) {
+						foreach ($events as $event) {
 							$start_date = strtotime($event['startdate']);
 							$end_date = strtotime($event['enddate']);
-							$current_date = strtotime($verif_date);
+							$current_date_time = strtotime($current_date);
 
-							if ($current_date >= $start_date && $current_date <= $end_date) {
-								$outDays .= generateIndicator($event);
+							if ($current_date_time >= $start_date && $current_date_time <= $end_date) {
+								$outDays .= generateIndicator($event, $currentDay);
 							}
 						}
 					}
@@ -204,81 +207,86 @@ function showCalendar($month = null): string
 
 	return $outDays;
 }
-
-function showCalendarFull(): string
-{
-	$out = "";
-	$count = 1;
-
-	for ($j = 0; $j < 4; $j++) {
-		$out .= "<tr>";
-		for ($i = 0; $i < 3; $i++) {
-			$cal = showCalendar(str_pad($count, 2, '0', STR_PAD_LEFT));
-			$out .= "<td style='border: none'>{$cal}</td>";
-			$count++;
-		}
-
-		$out .= "</tr>";
-	}
-
-	return $out;
-}
-
 ?>
 
 <div class="BoxContent" style="background-image:url(https://static.tibia.com/images/global/content/scroll.gif);">
-  <div id="eventscheduletablecontainer">
-	<div class="TableContainer">
-	  <div class="CaptionContainer">
-		<div class="CaptionInnerContainer">
-		  <span class="CaptionEdgeLeftTop"
-				style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
-		  <span class="CaptionEdgeRightTop"
-				style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
-		  <span class="CaptionBorderTop"
-				style="background-image:url(https://static.tibia.com/images/global/content/table-headline-border.gif);"></span>
-		  <span class="CaptionVerticalLeft"
-				style="background-image:url(https://static.tibia.com/images/global/content/box-frame-vertical.gif);"></span>
-		  <div class="Text">
-			<div class="eventscheduleheadertop">
-			  <div class="eventscheduleheaderblockleft">
-				<div class="eventscheduleheaderdateblock">
-				  <span class="eventscheduleheaderleft"></span><?= date('M Y') ?>
-				  <span class="eventscheduleheaderright">
-					<?php /*<a href="<?= '?eventcalendar&mes=' . $month . '&dia=' . ($currentDay + 1) ?>" style="color:white;">»</a>*/ ?>
-				  </span>
+	<div id="eventscheduletablecontainer">
+		<div class="TableContainer">
+			<div class="CaptionContainer">
+				<div class="CaptionInnerContainer">
+					<span class="CaptionEdgeLeftTop" style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
+					<span class="CaptionEdgeRightTop" style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
+					<span class="CaptionBorderTop" style="background-image:url(https://static.tibia.com/images/global/content/table-headline-border.gif);"></span>
+					<span class="CaptionVerticalLeft" style="background-image:url(https://static.tibia.com/images/global/content/box-frame-vertical.gif);"></span>
+
+					<div class="Text">
+						<div class="eventscheduleheadertop">
+							<div class="eventscheduleheaderblockleft">
+								<div class="eventscheduleheaderdateblock">
+									<span class="eventscheduleheaderleft">
+										<?php
+
+										$year = $getYear;
+										$month = $getMonth - 1;
+
+										if ( $getMonth == 1 ) {
+											$year = $getYear - 1;
+											$month = 12;
+										}
+
+										if ($getMonth > $currentMonth || $getYear > $currentYear) {
+											echo '<a href="' . getLink('event-schedule') . '?year=' . $year .
+										'&month=' . $month . '" style = "color:white;" > «</a>';
+										}
+
+										?>
+									</span>
+									<?= $monthName . ' ' . $getYear; ?>
+									<span class="eventscheduleheaderright">
+
+										<?php
+
+										$year = $getYear;
+										$month = $getMonth + 1;
+
+										if ( $getMonth == 12 ) {
+											$year = $getYear + 1;
+											$month = 1;
+										}
+
+										echo '<a href="' . getLink('event-schedule') . '?year=' . $year . '&month=' . $month . '" style = "color:white;" > »</a>';
+
+										?>
+									</span>
+								</div>
+							</div>
+							<div class="eventscheduleheaderblockright"><?= date('Y-m-d H:i') ?></div>
+						</div>
+					</div>
+
+					<span class="CaptionVerticalRight" style="background-image:url(https://static.tibia.com/images/global/content/box-frame-vertical.gif);"></span>
+					<span class="CaptionBorderBottom" style="background-image:url(https://static.tibia.com/images/global/content/table-headline-border.gif);"></span>
+					<span class="CaptionEdgeLeftBottom" style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
+					<span class="CaptionEdgeRightBottom" style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
 				</div>
-			  </div>
-			  <div class="eventscheduleheaderblockright"><?= date('Y-m-d H:i') ?></div>
 			</div>
-		  </div>
-		  <span class="CaptionVerticalRight"
-				style="background-image:url(https://static.tibia.com/images/global/content/box-frame-vertical.gif);"></span>
-		  <span class="CaptionBorderBottom"
-				style="background-image:url(https://static.tibia.com/images/global/content/table-headline-border.gif);"></span>
-		  <span class="CaptionEdgeLeftBottom"
-				style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
-		  <span class="CaptionEdgeRightBottom"
-				style="background-image:url(https://static.tibia.com/images/global/content/box-frame-edge.gif);"></span>
-		</div>
-	  </div>
-	  <table class="Table1" cellpadding="0" cellspacing="0" style="background-color: rgb(241, 224, 197);">
-		<tbody>
-		<tr>
-		  <td>
-			<div class="InnerTableContainer" style="padding: 10px;">
-			  <table style="width:100%;" id="eventscheduletable">
+			<table class="Table1" cellpadding="0" cellspacing="0" style="background-color: rgb(241, 224, 197);">
 				<tbody>
-				<?= showCalendar() ?>
+				<tr>
+					<td>
+						<div class="InnerTableContainer" style="padding: 10px;">
+							<table style="width:100%;" id="eventscheduletable">
+								<tbody>
+								<?= showCalendar($getMonth, $getYear) ?>
+								</tbody>
+							</table>
+						</div>
+					</td>
+				</tr>
 				</tbody>
-			  </table>
-			</div>
-		  </td>
-		</tr>
-		</tbody>
-	  </table>
+			</table>
+		</div>
 	</div>
-  </div>
-  <br>
-  <div>* Event starts/ends at server save of this day.</div>
+	<br>
+	<div>* Event starts/ends at server save of this day.</div>
 </div>
