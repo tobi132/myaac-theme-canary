d922f1f1665e03f06254d05d273dcf99703e7655
tobi132 <tobi132@gmx.net>
2026-01-16 23:51:20 +0100

    theme-canary: v2.0
    * Fix online, characters & highscores for latest myaac v2.0-dev (Monk support)
    * Remove routes hook, is useless in this myaac version

--- plugins/theme-canary/hooks/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MYAAC') or die('Direct access not allowed!');
-
-global $template_name;
-
-if ($template_name === 'canary') {
-	$args[] = [['get', 'post'], 'characters[/{name:[A-Za-z0-9-_%+\' \[\]]+}]', 'plugins/theme-canary/pages/characters.php', 500];
-}
